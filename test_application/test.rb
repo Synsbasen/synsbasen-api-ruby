c83240dd7866453fbdec7c95a03c417d8cbfbedd
Jimmy Poulsen <jimmy@poulsen.dk>
2023-11-10 21:06:13 +0100

    Test application for development purposes

---
+++ test_application/test.rb
@@ -0,0 +1,59 @@
+require 'bundler/setup'
+Bundler.require
+
+require 'dotenv'
+Dotenv.load('./.env')
+
+SynsbasenApi.configure do |config|
+  config.api_key = ENV.fetch("API_KEY")
+end
+
+# Vehicles
+puts "Vehicles"
+begin
+  response = SynsbasenApi::Vehicle.find_by_registration("AS67902")
+  puts response.data[:registration]
+rescue => e
+  binding.irb
+end
+
+response = SynsbasenApi::Vehicle.search({ registration: "AS679" })
+puts response.data.map { |v| v[:registration] }
+
+puts ""
+
+puts "Inspections"
+
+# Inspections
+test_center_ids = [1522, 284]
+start_date = "2023-01-01"
+end_date = "2023-12-31"
+
+args = {
+  query: {
+    inspection_test_center_id_in: test_center_ids,
+    date_gteq: start_date,
+    date_lteq: end_date,
+  },
+  method: 'SELECT',
+  per_page: 20,
+  page: 1,
+  sorts: 'date ASC'
+}
+
+response = SynsbasenApi::Inspection.search(args)
+puts response.data.map { |i| i[:date] + " " + i[:time] }
+
+puts ""
+
+# Inspection test_application centers
+puts "Inspection test_application centers"
+response = SynsbasenApi::InspectionTestCenter.all
+puts response.data.first(10).map { |i| i[:name] }
+
+puts ""
+
+# Brands
+puts "Brands"
+response = SynsbasenApi::Brand.all
+puts response.data.first(10).map { |i| i[:name] }
--- todo.md
+++ todo.md
@@ -1,10 +1,10 @@
 # TODO
 
 ## Version 1
-- [ ] Should just return the data as is
-- [ ] Handle errors with custom classes
+- [x] Should just return the data as is in an `ApiResponse` object
+- [x] Handle errors with custom classes
 - [ ] Add tests
 - [ ] Write documentation
 
 ## Version 2
-- [ ] Should utilize `ApiResponse`, `ApiObject` and `ListObject` classes similar to [Stripe's Ruby gem](https://github.com/stripe/stripe-ruby)
+- [ ] Should utilize `ApiResponse`, `ApiObject` and `ListObject` objects similar to [Stripe's Ruby gem](https://github.com/stripe/stripe-ruby)
