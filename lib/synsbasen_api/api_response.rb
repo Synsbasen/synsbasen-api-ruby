c50e3e2e9dfc98f8eb9081152777ea154b5f3da6
Jimmy Poulsen <jimmy@poulsen.dk>
2023-11-10 21:35:04 +0100

    Add has_more attr_reader to ApiResponse

--- lib/synsbasen_api/api_response.rb
+++ lib/synsbasen_api/api_response.rb
@@ -1,10 +1,11 @@
 module SynsbasenApi
   class ApiResponse
-    attr_reader :data, :cost
+    attr_reader :data, :cost, :has_more
 
     def initialize(response)
       @data = response[:data]
       @cost = response[:cost]
+      @has_more = response[:has_more] || false
     end
   end
 end
