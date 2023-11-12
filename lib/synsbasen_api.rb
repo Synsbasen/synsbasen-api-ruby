73d8d42a31368675d283c5271bf20138d91e931a
Jimmy Poulsen <jimmy@poulsen.dk>
2023-11-12 13:43:59 +0100

    Update version

--- lib/synsbasen_api.rb
+++ lib/synsbasen_api.rb
@@ -10,30 +10,28 @@ require "synsbasen_api/resources/vehicle"
 require "synsbasen_api/resources/variant"
 require "synsbasen_api/resources/version"
 
+# The `SynsbasenApi` module provides a configuration mechanism and requires various
+# classes and modules related to interacting with the Synsbasen API.
 module SynsbasenApi
-  # The `SynsbasenApi` module provides a configuration mechanism and requires various
-  # classes and modules related to interacting with the Synsbasen API.
-  module SynsbasenApi
-    # An array of required configuration keys.
-    REQUIRED_CONFIGS = %i[api_key]
+  # An array of required configuration keys.
+  REQUIRED_CONFIGS = %i[api_key]
 
-    # Configures the Synsbasen API client with the specified options.
-    #
-    # @yieldparam config [OpenStruct] The configuration object.
-    def self.configure
-      @config ||= OpenStruct.new
-      yield(@config) if block_given?
+  # Configures the Synsbasen API client with the specified options.
+  #
+  # @yieldparam config [OpenStruct] The configuration object.
+  def self.configure
+    @config ||= OpenStruct.new
+    yield(@config) if block_given?
 
-      raise "Missing configuration. Required configurations are #{REQUIRED_CONFIGS}" unless REQUIRED_CONFIGS.all? { |c| @config[c] }
+    raise "Missing configuration. Required configurations are #{REQUIRED_CONFIGS}" unless REQUIRED_CONFIGS.all? { |c| @config[c] }
 
-      @config
-    end
+    @config
+  end
 
-    # Retrieves the current configuration or configures the Synsbasen API client with default options.
-    #
-    # @return [OpenStruct] The configuration object.
-    def self.config
-      @config || configure
-    end
+  # Retrieves the current configuration or configures the Synsbasen API client with default options.
+  #
+  # @return [OpenStruct] The configuration object.
+  def self.config
+    @config || configure
   end
 end
