458296c889286cc6c5cf2e871f426dff51682994
Jimmy Poulsen <jimmy@poulsen.dk>
2024-05-06 20:20:05 +0200

    Add YARDs for Resource#resource_name

--- lib/synsbasen_api/resource.rb
+++ lib/synsbasen_api/resource.rb
@@ -5,6 +5,10 @@ module SynsbasenApi
     class << self
       private
 
+      # Define the resource name
+      #
+      # @return [String]
+      # @raise [NotImplementedError] if the method is not implemented by the subclass
       def resource_name
         raise NotImplementedError, 'Subclasses must implement this method'
       end
