ee2725918ded952fddd69b50dbd7a9b66e3e01be
Jimmy Poulsen <jimmy@poulsen.dk>
2024-05-06 21:03:24 +0200

    Extend Searchable in Brand, Model, Variant, and Version resources

--- lib/synsbasen_api/resources/brand.rb
+++ lib/synsbasen_api/resources/brand.rb
@@ -5,6 +5,7 @@ module SynsbasenApi
   # endpoints in the Synsbasen API.
   class Brand < Resource
     extend Findable
+    extend Searchable
 
     class << self
       # Retrieves information about all brands.
